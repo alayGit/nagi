745fadd7e37b40ed9e2330514560832ecc35c2af
andrew <andrewjohnlay@gmail.com>
2022-12-17 17:20:47 +1100

    Adding view

---
+++ src/view.h
@@ -0,0 +1,104 @@
+/**************************************************************************
+** VIEW.H
+**************************************************************************/
+
+#ifndef _VIEW_H_
+#define _VIEW_H_
+
+#include "general.h"
+#include "stub.h"
+#include "memoryManager.h"
+
+typedef struct {
+   byte width;
+   byte height;
+   byte transparency;
+   BITMAP *bmp;
+} Cel;
+
+typedef struct {
+   byte numberOfCels;
+   Cel *cels;
+} Loop;
+
+typedef struct {
+   boolean loaded;
+   byte numberOfLoops;
+   Loop *loops;
+   char *description;
+} View;
+
+#define DRAWN         0x0001
+#define IGNOREBLOCKS  0x0002
+#define FIXEDPRIORITY 0x0004
+#define IGNOREHORIZON 0x0008
+#define UPDATE        0x0010
+#define CYCLING       0x0020
+#define ANIMATED      0x0040
+#define MOTION        0x0080
+#define ONWATER       0x0100
+#define IGNOREOBJECTS 0x0200
+#define ONLAND        0x0800
+#define FIXLOOP       0x2000
+
+typedef struct {
+   byte stepTime;
+   byte stepTimeCount;
+   word xPos;
+   word yPos;
+   byte currentView;
+   View *viewData;             /* This pointer points to the loaded view */
+   byte currentLoop;
+   byte numberOfLoops;
+   Loop *loopData;             /* ditto */
+   byte currentCel;
+   byte numberOfCels;
+   Cel *celData;              /* ditto */
+   BITMAP *bgPic;             /* Storage for background behind drawn view */
+   BITMAP *bgPri;
+   word bgX;                  /* Position to place background bmp */
+   word bgY;
+   word xsize;
+   word ysize;
+   byte stepSize;
+   byte cycleTime;
+   byte cycleTimeCount;
+   byte direction;
+   byte motion;
+   byte cycleStatus;
+   byte priority;
+   word flags;
+   byte param1;
+   byte param2;
+   byte param3;
+   byte param4;
+} ViewTable;
+
+#define TABLESIZE  20  // 100
+extern ViewTable viewtab[];
+
+#define MAXVIEW  256
+extern View loadedViews[];
+
+extern BITMAP *spriteScreen;
+
+extern void getViewTab(ViewTable* returnedViewTab, byte viewTabNumber);
+extern void setViewTab(ViewTable* viewTab, byte viewTabNumber);
+
+void loadViewFile(byte viewNum);
+void discardView(byte viewNum);
+void addViewToTable(byte entryNum, byte viewNum);
+
+extern void updateObj(int entryNum);
+extern void setCel(byte entryNum, byte celNum);
+extern void setLoop(byte entryNum, byte loopNum);
+extern void drawObject(int entryNum);
+extern void addToPic(int vNum, int lNum, int cNum, int x, int y, int pNum, int bCol);
+extern void showObjectState(int objNum);
+extern void resetViews();
+extern void calcObjMotion();
+extern void initViews();
+extern void initObjects();
+
+#endif   /* _VIEW_H_ */
+
